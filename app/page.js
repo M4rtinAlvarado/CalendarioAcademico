9ea6e717ea5d8c2b5a1283758659a3065c396a79
AndresMardonesUACH <andymardones1@gmail.com>
2023-10-14 21:18:32 -0300

    Pruebas gráficas

--- app/page.js
+++ app/page.js
@@ -1,19 +1,33 @@
 "use client"
 import FullCalendar from "@fullcalendar/react"
+import interactionPlugin from '@fullcalendar/interaction';
 import multiMonthPlugin from "@fullcalendar/multimonth"
 
 
 
 export default function Home() {
   return (
-    <FullCalendar
-      plugins={[ multiMonthPlugin ]}
-      initialView="multiMonthYear"
-      firstDay={1}
-      events = {[
-        {title:"a", start: '2023-10-13', end: '2023-10-16'},
-        {title:"b", start: '2023-10-13', end: '2023-10-17'},
-      ]}
-    />
+    <div className="main">
+      <h1>CALENDARIO UACH 2023</h1>
+      <div className="contenedor">
+      
+      <FullCalendar 
+        eventBackgroundColor=""
+        headerToolbar = "false"
+        aspectRatio={2.77}
+        height={610}
+        multiMonthMinWidth={500}
+        plugins={[ multiMonthPlugin ]}
+        initialView="multiMonthYear"
+        firstDay={1}
+        locale = "esLocale"
+        multiMonthMaxColumns = "1"
+        events = {[
+          {title:"a", start: '2023-10-13', className: "a"},
+          {title:"b", start: '2023-10-15', end: '2023-10-17', backgroundColor: "blue"},
+        ]}
+      />
+      </div>
+    </div>
   )
 }
