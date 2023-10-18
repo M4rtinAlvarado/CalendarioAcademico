1a3e608778a889acec29674b502ba7da50640bb4
M4rtinAlvarado <martin.alvarado@alumnos.uach.cl>
2023-10-18 11:38:23 -0300

    Inicios de filtros 1.0

--- app/page.js
+++ app/page.js
@@ -1,11 +1,30 @@
 "use client"
+import { useState } from 'react';
 import FullCalendar from "@fullcalendar/react"
-import interactionPlugin from '@fullcalendar/interaction';
-import multiMonthPlugin from "@fullcalendar/multimonth"
+import multiMonthPlugin from "@fullcalendar/multimonth";
+import eventos, { events } from './api/eventos';
+
 
 
 
 export default function Home() {
+  const allCategories = [
+		'All',
+		...new Set(events.map(evento => evento.type)),
+	];
+
+	const [categories, setCategories] = useState(allCategories);
+	const [articles, setArticles] = useState(eventos);
+
+	const filterCategory = (category) => {
+		if (category === 'All'){
+			setArticles(data)
+			return
+		}
+
+		const filteredData = data.filter(article => article.category === category);
+		setArticles(filteredData)
+	}
   return (
     <div className="main">
       <h1>CALENDARIO UACH 2023</h1>
@@ -27,6 +46,10 @@ export default function Home() {
           {title:"b", start: '2023-10-15', end: '2023-10-17', backgroundColor: "blue"},
         ]}
       />
+
+      </div>
+      <div>
+        {categories}
       </div>
     </div>
   )
