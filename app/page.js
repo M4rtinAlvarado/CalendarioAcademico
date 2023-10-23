45c56b3aa5373891e5cb65833a2228a8eb4bd5ea
AndresMardonesUACH <andymardones1@gmail.com>
2023-10-22 21:57:34 -0300

    Avances pagina principal

--- app/page.js
+++ app/page.js
@@ -4,6 +4,8 @@ import Link from 'next/link'
 import interactionPlugin from '@fullcalendar/interaction';
 import multiMonthPlugin from "@fullcalendar/multimonth"
 import dayGridPlugin from '@fullcalendar/daygrid'
+import {eventos} from '@/app/api/eventos' 
+import { Button, ButtonGroup } from '@chakra-ui/react'
 
 
 
@@ -26,6 +28,15 @@ function buildValidRange() {
 export default function Home() {
   return (
     <div className="main-home">
+      <div className="contenedor-filtros">
+          <h1 className="title-filtros">Filtros</h1>
+          <Button variant="outline" margin={5} backgroundColor={"white"}>Filtro1</Button>
+          <Button variant="outline" margin={5} backgroundColor={"white"}>Filtro2</Button>
+          <Button variant="outline" margin={5} backgroundColor={"white"}>Filtro3</Button>
+          <Button variant="outline" margin={5} backgroundColor={"white"}>Filtro4</Button>
+          <Button variant="outline" margin={5} backgroundColor={"white"}>Filtro5</Button>
+          <Button variant="outline" margin={5} backgroundColor={"white"}>Filtro6</Button>
+      </div>
       <div className="contenedor-calendario">
         <FullCalendar 
           headerToolbar = {buildToolbar()}
@@ -38,27 +49,7 @@ export default function Home() {
           handleWindowResize='true'
           locale = "esLocale"
 
-          events = {[   {description:"Segundo período de matrícula Proceso de Admisión 2023 (Listas de espera, lista adicional de espera, BEA, PACE e ingresos especiales).", 
-          title:"Matricula", className: "Financiero",sem: "Primer periodo",start: "2023-01-21", end: "2023-01-27" },
-         {description:"Segundo período para que las y los profesores responsables actualicen las características y publiquen el programa de sus asignaturas del primer semestre 2023.",  
-          title:"Asignatura", className: "Académico",sem: "Primer periodo",start: "2023-03-02" ,end: "2023-03-13"},
-         {description:"Bienvenida e Inducción para estudiantes Ingreso 2023 provenientes de Programas PACE, Propedéutico y Ranking 1000.", 
-          title:"Bienvenida", className: "Social",campus: ["Valdivia","Puerto Montt"],sem: "Primer periodo",start: "2023-03-03" ,end: "2023-03-07"},
-         {description:"Talleres de inducción de la Unidad de Apoyo al Aprendizaje (UAAEP) para Ingresos 2023.", 
-          title:"Talleres", className: "Otros",sem: "Primer semestre",start: "2023-03-08" ,end: "2023-03-30"},
-         {description:"Publicación de resultados de asignación de Gratuidad, Becas y Créditospara postulantes FUAS www.becasycreditos.cl",
-          title:"FUAS", className: "Financiero",sem: "Primer semestre",start: "2023-03-09" },
-         {description:"Ceremonia de celebración de Aniversario de la Universidad en el Campus Patagonia.",
-          title:"Ceremonia", className: "Social",campus: "Patagonia",sem: "Segundo periodo",start: "2023-09-11"},
-         {description:"Semana de pausa estudiantil",
-          title:"Receso", className: "Otros",sem: "Segundo periodo",start: "2023-09-14" ,end: "2023-09-22"},
-         {description:"Cuarta publicación de resultados de Asignación de Gratuidad, Becas de Arancel y Créditos en sitio www.becasycreditos.cl",
-          title:"Beneficios", className: "Financiero",sem: "Segundo periodo",start: "2023-10-03"}, 
-         {description:"Último día para que estudiantes de carreras semestrales presentenlas solicitudes de anulación de semestre, a través de www.uach.cl/alumnos.",
-          title:"Anulación", className: "Académico",sem: "Segundo periodo",start: "2023-10-26"},
-         {description:"Último día de clases del segundo semestre.",
-          title:"Término", className: "Académico",sem: "Segundo periodo",start: "2023-12-01" },
-     ]}
+          events = {eventos}
 
         />
       </div>
