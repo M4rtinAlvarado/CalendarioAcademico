8dce88a8c225197f988d7d42c20f1a1c7025ad6b
M4rtinAlvarado <martin.alvarado@alumnos.uach.cl>
2023-11-05 23:02:37 -0300

    Los botones de los filtros cambian de color

--- app/page.js
+++ app/page.js
@@ -1,11 +1,27 @@
 "use client"
 import FullCalendar from "@fullcalendar/react"
-import {  useEffect, useState } from 'react';
+import Link from 'next/link'
+import interactionPlugin from '@fullcalendar/interaction'
 import multiMonthPlugin from "@fullcalendar/multimonth"
 import dayGridPlugin from '@fullcalendar/daygrid'
-import { Stack, HStack, VStack } from '@chakra-ui/react';
-import { Button, ButtonGroup } from '@chakra-ui/react';
-import { allCampus, typevents, events} from './api/eventos';
+import { allCampus, typevents, events } from './api/eventos'
+import { Stack, HStack, VStack } from '@chakra-ui/react'
+import {
+  Input,
+  Button,
+  ButtonGroup,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from '@chakra-ui/react'
+import { useEffect, useState } from "react"
+import Boton from "@/app/components/Boton";
+
 
 function buildToolbar() {
   return {
@@ -21,18 +37,48 @@ function buildValidRange() {
     end: '2024-01-01'
   }
 }
+function buildEstados(typevents){
+  let estados = [];
+  for(let i = 0; i < typevents.length; i++){
+    estados.push([typevents[i], false]);
+  }
+  return estados;
+}
+
+function estadosIndex(category, estados){
+  for(let i = 0; i < estados.length; i++){
+    if(estados[i][0] === category){
+      return i;
+    }
+  }
+}
+
+function changeState(category, estados){
+  let aux = estados;
+  aux[estadosIndex(category, estados)][1] = !(aux[estadosIndex(category, estados)][1]);
+  return aux;
+}
+
 
 export default function Home() {
-
-	const [selectedFilters, setSelectedFilters] = useState([])
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [eventTitle, setEventTitle,] = useState("")
+  const [eventStartDate, setEventStartDate] = useState("")
+  const [eventDescription, setEventDescription] = useState("")
+  const [eventClassName, setEventClassName] = useState("")
+  const [eventEndDate, setEventEndDate] = useState("")
+	const [selectedFilters, setSelectedFilters] = useState(typevents.concat(allCampus))
 	const [eventos, setEventos] = useState(events);
+  const [estado, setEstado] = useState(buildEstados(typevents.concat(allCampus)))
 
   const handleFilterButtonClick = (selectedCategory) => {
     if (selectedFilters.includes(selectedCategory)) {
       let filters = selectedFilters.filter((a) => a !== selectedCategory)
       setSelectedFilters(filters)
+      setEstado(changeState(selectedCategory, estado))
     } else {
       setSelectedFilters([...selectedFilters, selectedCategory])
+      setEstado(changeState(selectedCategory, estado))
     }
   };
 
@@ -93,43 +139,82 @@ else if (campus.length > 0 && type.length === 0){
   setEventos(items)
 }
 else {
-    setEventos(events)
+    setEventos([])
   }
 }
-console.log(eventos)
-console.log(selectedFilters)
   
   return (
     <div className="main-home">
-    <div className="contenedor-filtros">
-      <h2 className="title-filtros">Filtros</h2>
-      <Stack spacing={2} align='center'>
+      <div className="contenedor-filtros">
+        <h1 className="title-filtros">Filtros</h1>
         {typevents.map((category) => (
-          <Button onClick={() => handleFilterButtonClick(category)} className={category}>
-            {category}
-          </Button>
+          <Boton clase ={category} state = {estado[estadosIndex(category, estado)][1]} click = {() => handleFilterButtonClick(category)} />
         ))}
         {allCampus.map((category) => (
-          <Button onClick={() => handleFilterButtonClick(category)}>
-            {category}
-          </Button>
+          <Boton clase ={category} state = {estado[estadosIndex(category, estado)][1]} click = {() => handleFilterButtonClick(category)} />
         ))}
-      </Stack>
+      </div>
+      <div className="contenedor-calendario">
+      
+        <FullCalendar
+          selectable={true}
+          eventClick={function (info) {
+            setEventTitle(info.event.title) 
+            setEventStartDate(info.event.start.toLocaleDateString())
+            if(info.event.end == null) setEventEndDate(info.event.start.toLocaleDateString())
+            else{ 
+              setEventEndDate(info.event.end.toLocaleDateString());
+            }
+            setEventClassName(info.event.classNames[0])
+            setEventDescription(info.event.extendedProps.description)
+            onOpen()
+          }}
+          headerToolbar={buildToolbar()}
+          validRange={buildValidRange()}
+          aspectRatio={1}
+          height={660}
+          expandRows={'true'}
+          plugins={[multiMonthPlugin, dayGridPlugin]}
+          initialView="multiMonthYear"
+          firstDay={1}
+          handleWindowResize='true'
+          locale="esLocale"
+
+          events={eventos}
+        />
+        <>
+          <Modal isOpen={isOpen} onClose={onClose} size={"4xl"} motionPreset="scale">
+          <ModalOverlay/>
+          <ModalContent>
+            <ModalHeader>{eventTitle}</ModalHeader>
+            <ModalCloseButton color={"white"} size={"lg"}/>
+            <ModalBody>
+            <div className="div-default">
+                <div className="fecha">
+                    <b><h3>Fecha: {eventStartDate} - {eventEndDate}</h3></b>
+                </div>
+            </div>
+            <b><h3>Categoría:</h3></b>
+            <div className="div-default">
+                <div className={eventClassName}>{eventClassName}</div>
+            </div>
+            <div className="div-default">
+                <p>{eventDescription}</p>
+            </div>
+            <b><h3>Comentarios:</h3></b>
+            <div className="div-comentarios">
+                <div className="comentario">Comentario1</div>
+                <div className="form">
+                    <Input placeholder='Nuevo Comentario'  width= '90%' borderColor={'black'} marginRight={'2%'} />
+                    <Button variant='outline' borderColor={'black'} backgroundColor={'gray.300'}>Enviar</Button>
+                </div>
+            </div>
+            </ModalBody>
+          </ModalContent>
+        </Modal>
+      </>
+      </div>
+
     </div>
-    <div className="contenedor-calendario">
-      <FullCalendar
-        headerToolbar = {buildToolbar()}
-        validRange={buildValidRange()}
-        aspectRatio={1.3}
-        height={660}
-        plugins={[ multiMonthPlugin, dayGridPlugin ]}
-        initialView="multiMonthYear"
-        firstDay={1}
-        handleWindowResize='true'
-        locale = "esLocale"
-        events = {eventos}
-      />
-    </div>
-  </div>
 )
 }
